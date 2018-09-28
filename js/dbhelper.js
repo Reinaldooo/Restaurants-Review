fae40efffadfc29a11af8ee2bad2d3bb8c598fb4
Your NameReinaldo <rewifetri@gmail.com>
2018-09-28 14:51:29 -0300

    DB address fix III

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
+    const port = 8080 // Change this to your server port
     return `https://reinaldooo.github.io/mws-restaurant-stage-1/data/restaurants.json`;
   }
 
