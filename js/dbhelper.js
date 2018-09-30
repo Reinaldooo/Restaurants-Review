95f15dcc0044184bc0603285bc832ed3671128b8
Your NameReinaldo <rewifetri@gmail.com>
2018-09-30 13:23:17 -0300

    testing dbport setting III

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -163,7 +163,21 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    const isLocalhost = Boolean(
+      window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+    );
+    if (isLocalhost) {
+      return (`/img/${restaurant.photograph}`);
+    } else {
+      /Restaurants-Review/
+      return (`/Restaurants-Review/img/${restaurant.photograph}`)
+    }
   }
 
   /**
