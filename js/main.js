32507fbf616f32f549c20a19ebf1c08b01225ba9
Your NameReinaldo <rewifetri@gmail.com>
2018-09-30 14:08:45 -0300

    fixed top and bottom home links href while in githubpages

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   initMap(); // added 
   fetchNeighborhoods();
   fetchCuisines();
+  fixLinksGithubPages();
 });
 
 /**
@@ -232,18 +233,20 @@ if ('serviceWorker' in navigator) {
   });
 };
 
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-  // [::1] is the IPv6 localhost address.
-  window.location.hostname === '[::1]' ||
-  // 127.0.0.1/8 is considered localhost for IPv4.
-  window.location.hostname.match(
-    /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-  )
-);
-
-if (!isLocalhost) {
-  document.getElementById('index-bottom-home-link').setAttribute('href', "/Restaurants-Review/")
-  document.getElementById('index-top-home-link').setAttribute('href', "/Restaurants-Review/")
-};
+fixLinksGithubPages = () => {
+  const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+  );
+  
+  if (!isLocalhost) {
+    document.getElementById('index-bottom-home-link').setAttribute('href', "/Restaurants-Review/")
+    document.getElementById('index-top-home-link').setAttribute('href', "/Restaurants-Review/")
+  };
+}
 
