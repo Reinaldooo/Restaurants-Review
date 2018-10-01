bca2cc7df187b0313f0a497e4d48f61b8ddc5cc4
Your NameReinaldo <rewifetri@gmail.com>
2018-10-01 12:57:24 -0300

    fix request error on sw_gpages.js cacheAll path

--- sw_gpages.js
+++ sw_gpages.js
@@ -6,7 +6,7 @@ self.addEventListener('install', (event) => {
     //open up a new cache and store the important parts    
     caches.open(staticCacheName).then((cache) => {
       return cache.addAll([
-        '/',
+        '/Restaurants-Review',
         'index.html',
         'css/main.css',
         'js/dbhelper.js',
